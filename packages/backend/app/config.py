4f6a928ca16785e34fe5b53e37aa92c45188e6e2
nedhmn <hermannned@gmail.com>
2025-08-15 14:13:15 -0400

    feat: config settings

---
+++ packages/backend/app/config.py
@@ -0,0 +1,19 @@
+from typing import Literal
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+
+    APP_TITLE: str = Field(default="Duel Insights API")
+    API_PREFIX: str = Field(default="/api/v1")
+    ENVIRONMENT: Literal["local", "staging", "production"] = Field(default="local")
+
+
+settings = Settings()
